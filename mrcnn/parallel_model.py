3b5820778677eecd0da7b22929206cae7bea5ea7
lnx2000 <omkarsamilkanthwar2000@gmail.com>
2020-08-06 15:23:49 +0530

    revert back

--- mrcnn/parallel_model.py
+++ mrcnn/parallel_model.py
@@ -13,10 +13,10 @@ https://github.com/avolkov1/keras_experiments/blob/master/keras_exp/multigpu/
 https://github.com/fchollet/keras/blob/master/keras/utils/training_utils.py
 """
 
-import tensorflow.compat.v1 as tf
-import tensorflow.keras.backend as K
-import tensorflow.keras.layers as KL
-import tensorflow.keras.models as KM
+import tensorflow as tf
+import keras.backend as K
+import keras.layers as KL
+import keras.models as KM
 
 
 class ParallelModel(KM.Model):
@@ -113,9 +113,9 @@ if __name__ == "__main__":
 
     import os
     import numpy as np
-    from tensorflow import keras
-    from tensorflow.keras.datasets import mnist
-    from tensorflow.keras.preprocessing.image import ImageDataGenerator
+    import keras.optimizers
+    from keras.datasets import mnist
+    from keras.preprocessing.image import ImageDataGenerator
 
     GPU_COUNT = 2
 
